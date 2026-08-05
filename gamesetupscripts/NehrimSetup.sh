61f36c131ce2331d3e8b5cdef1ea7944fd7b08cf
Thomas Hayes <thomashayes@thomashayes.dev>
2026-08-04 21:06:38 -0400

    updated nehrim installer

--- gamesetupscripts/NehrimSetup.sh
+++ gamesetupscripts/NehrimSetup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sudo xbps-install -Su \
-&& sudo xbps-install -S wine winetricks wine-32bit wine-common wine-devel wine-devel-32bit wine-gecko wine-mono wine-tools p7zip lutris \
+&& sudo xbps-install -S wine winetricks wine-32bit wine-common wine-devel wine-devel-32bit wine-gecko wine-mono wine-tools p7zip \
 && mkdir /home/$USER/Nehrim \
 && WINEPREFIX=/home/$USER/Nehrim winecfg -v win7 \
 && export WINEPREFIX=/home/$USER/Nehrim \
-&& WINEPREFIX=/home/$USER/Nehrim winetricks --force --unattended dotnet48 dotnetdesktop6 vcrun2015 vcrun2017 vcrun2019 vcrun2022 dxvk d3dx11_43 d3dcompiler_43 d3dcompiler_47 faudio \
+&& WINEPREFIX=/home/$USER/Nehrim winetricks --force --unattended dotnet48 dotnetdesktop6 vcrun2015 vcrun2017 vcrun2019 vcrun2022 dxvk d3dx9_43 d3dcompiler_43 d3dcompiler_47 faudio \
 && WINEPREFIX=/home/$USER/Nehrim winecfg -v win7 \
-&& WINEPREFIX=/home/$USER/Nehrim wine /home/$USER/Games/Enderal_Mods/'setup_the_elder_scrolls_v_skyrim_special_edition_0.1.3905696_(64bit)_(70738).exe' \
-&& WINEPREFIX=/home/$USER/Nehrim wine /home/$USER/Games/Enderal_Mods/Mod.Organizer-2.5.2.exe \
+&& WINEPREFIX=/home/$USER/Nehrim wine "$PWD/setup_nehrim_at_fates_edge_1.0.0.0_(61681).exe" \
+&& WINEPREFIX=/home/$USER/Nehrim wine "$PWD/Mod Organizer 2-6-2-5-2-1772983502.exe" \
 && cp nehrim.sh /home/$USER/
---
+++ gamesetupscripts/nehrim.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+WINEPREFIX=/home/$USER/Nehrim sudo nice -n -15 su -c 'wine /home/$USER/Nehrim/drive_c/Modding/MO2/ModOrganizer.exe' $USER
+
