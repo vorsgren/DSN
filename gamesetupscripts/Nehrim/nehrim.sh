4656369ac099bb2bb23187a0063673783bcaaf26
Thomas Hayes <thomashayes@thomashayes.dev>
2026-08-25 20:17:35 -0400

    added autosavearchive nehrim

--- gamesetupscripts/Nehrim/nehrim.sh
+++ gamesetupscripts/Nehrim/nehrim.sh
@@ -6,4 +6,21 @@ export WINEPREFIX="$HOME/Nehrim"
 export STORE=gog
 
 cd "$HOME/Nehrim/drive_c/GOG Games/Nehrim At Fate's Edge GOG"
-ENABLE_GAMESCOPE_WSI=0 gamescope --backend sdl -w 1720 -h 720 -W3440 -H 1440 -F fsr --fsr-sharpness 2 -f --force-windows-fullscreen --force-grab-cursor -r 72 -- umu-run "NehrimLauncher.exe"
+sh -c 'gamescope --backend sdl -w 1720 -h 720 -W3440 -H 1440 -F fsr --fsr-sharpness 2 -f --force-windows-fullscreen --force-grab-cursor -r 72 -- umu-run "obse_loader.exe"; exit 0' 
+#gamescope --backend sdl -w 1720 -h 720 -W3440 -H 1440 -F fsr --fsr-sharpness 2 -f --force-windows-fullscreen --force-grab-cursor -r 72 -- umu-run "obse_loader.exe"
+
+#archive saves
+archivetime=$(date +"%Y-%m-%d_%H:%M:%S")
+newestfiletime=$(find "$HOME/Nehrim/drive_c/GOG Games/Nehrim At Fate's Edge GOG/Saves" -maxdepth 1 -type f -exec stat -c '%Y' {} \; | sort -n | tail -n 1)
+mkdir -p $HOME/Nehrim/BACKUPSAVES/$archivetime
+
+shopt -s nullglob #expand * to empty if no files
+for file in "$HOME/Nehrim/drive_c/GOG Games/Nehrim At Fate's Edge GOG/Saves"/*; do
+	currentfiletime=$(stat -c '%Y' "$file")
+	if [ "$currentfiletime" -eq "$newestfiletime" ]; then
+		:
+	else
+		mv "$file" "$HOME/Nehrim/BACKUPSAVES/$archivetime"
+	fi
+done
+shopt -u nullglob # Restore default * behavior
